38793d3dc83dad1954dd7aab9eea674a6f0c0e40
Patrik Eigenmann <patrik@Mac.attlocal.net>
2024-12-16 23:26:54 -0800

    submitting cdget.sh v00.14

--- cdget.sh
+++ cdget.sh
@@ -19,7 +19,7 @@
 # ------------------------------------------------------------------------------------
 # Change Log:
 # Tue 2023-06-13 File created.                                          Version: 00.01
-# Tue 2023-06-13 -?	    call the help manpages style.	                Version: 00.02
+# Tue 2023-06-13 "-?" or --help call the help manpages style.	        Version: 00.02
 # Tue 2023-06-13 -b	    bash files implemented.		                    Version: 00.03
 # Tue 2023-06-13 -c	    c files implemented.		                    Version: 00.04
 # Tue 2023-06-13 -h	    c\c++ header files implemented.	                Version: 00.05
@@ -31,7 +31,72 @@
 # Mon 2024-12-16 -mf	makefile template implemented.		            Version: 00.11
 # Mon 2024-12-16 Changed the path to user patrik, and added output.     Version: 00.12
 # Mon 2024-12-16 Corrected a bug: -mk was wrong it's -mf.               Version: 00.13
+# Mon 2024-12-16 Corrected the call of the help file.                   Version: 00.14
 # ************************************************************************************
+
+# ---------------------------------------------------------------------
+# display_help - displayes a helpful description alla man pages style.
+# A brief description of all the parameters and how to use the script.
+# ---------------------------------------------------------------------
+display_help () {
+    echo "NAME:"
+    echo "      cdget.sh - Version: $script_version"
+    echo " "
+    echo "SYNOPSIS:"
+    echo "      cdget.sh <option: -py,php,wphp,c,cpp,h,j,?> <path> <filename>"
+    echo " "
+    echo "DESCRIPTION:"
+    echo "      The script will make a copy of a specified template to make coder's"
+    echo "      life very easy. All parameters explained as follows!"
+    echo " "
+    echo "      -b"
+    echo "          Option -b to get a bash script template in the working folder."
+    echo " "
+    echo "      -c"
+    echo "          Option -c to get a c program template in the working folder."
+    echo " "
+    echo "      -h"
+    echo "          Option -h to get a c/c++ header template in the working folder."
+    echo " "
+    echo "      -mf"
+    echo "          Option -mf to get a makefile to use with my tool pmake in the"
+    echo "          working folder."
+    echo " "
+    echo "      -php"
+    echo "          Option -php to get a PHP template in the working folder."
+    echo " "
+    echo "      -py"
+    echo "          Option -py to get a python template in the working folder."
+    echo " "
+    echo "      -wphp"
+    echo "          Option -wphp to get a Website PHP template in the working folder."
+    echo " "
+    echo "      -j"
+    echo "          Option -j to get a java class template in the working folder."
+    echo " "
+    echo "      -cj"
+    echo "          Option -cj to get a c program template as a starter for a java"
+    echo "          app in the working folder."
+    echo " "
+    echo "      \"-?\" or --help"
+    echo "          Option -? displays this help file. With calling the help file,"
+    echo "          no other parameter are required!"
+    echo " "
+    echo "      <path>"
+    echo "          The secod parameter defines the path. It can be either ./ or a name."
+    echo " "
+    echo "      <filename>"
+    echo "          The third parameter defines the filename with a given name."
+    echo " "
+    echo "AUTHOR:"
+    echo "      Written by Patrik Eigenmann. My email is p.eigenmann@gmx.net."
+    echo " "
+    echo "COPYRIGHT:"
+    echo "      Copyright © 2023 Free Software Foundation, Inc. This software is under the GPLv3+ license."
+    echo "      GNU GPL version 3 or later https://gnu.org/licenses/gpl.html."
+    echo "      This is free software: you are free to change and redistribute it. There is NO WARRANTY, to the extent"
+    echo "      permitted by law."
+}
 
 script_version="00.13"
 x=1
@@ -129,73 +194,9 @@ fi
 # -----------------------------
 # Help section
 # -----------------------------
-if [ $language = "-?" ]; then
+if [ $language = "-?" ]|| [ "$language" = "--help" ]; then
 	display_help
 fi
-
-# ---------------------------------------------------------------------
-# display_help - displayes a helpful description alla man pages style.
-# A brief description of all the parameters and how to use the script.
-# ---------------------------------------------------------------------
-display_help () {
-    echo "NAME:"
-    echo "      cdget.sh - Version: $script_version"
-    echo " "
-    echo "SYNOPSIS:"
-    echo "      cdget.sh <option: -py,php,wphp,c,cpp,h,j,?> <path> <filename>"
-    echo " "
-    echo "DESCRIPTION:"
-    echo "      The script will make a copy of a specified template to make coder's"
-    echo "      life very easy. All parameters explained as follows!"
-    echo " "
-    echo "      -b"
-    echo "          Option -b to get a bash script template in the working folder."
-    echo " "
-    echo "      -c"
-    echo "          Option -c to get a c program template in the working folder."
-    echo " "
-    echo "      -h"
-    echo "          Option -h to get a c/c++ header template in the working folder."
-    echo " "
-    echo "      -mf"
-    echo "          Option -mf to get a makefile to use with my tool pmake in the"
-    echo "          working folder."
-    echo " "
-    echo "      -php"
-    echo "          Option -php to get a PHP template in the working folder."
-    echo " "
-    echo "      -py"
-    echo "          Option -py to get a python template in the working folder."
-    echo " "
-    echo "      -wphp"
-    echo "          Option -wphp to get a Website PHP template in the working folder."
-    echo " "
-    echo "      -j"
-    echo "          Option -j to get a java class template in the working folder."
-    echo " "
-    echo "      -cj"
-    echo "          Option -cj to get a c program template as a starter for a java"
-    echo "          app in the working folder."
-    echo " "
-    echo "      -?"
-    echo "          Option -? displays this help file. With calling the help file,"
-    echo "          no other parameter are required!"
-    echo " "
-    echo "      <path>"
-    echo "          The secod parameter defines the path. It can be either ./ or a name."
-    echo " "
-    echo "      <filename>"
-    echo "          The third parameter defines the filename with a given name."
-    echo " "
-    echo "AUTHOR:"
-    echo "      Written by Patrik Eigenmann. My email is p.eigenmann@gmx.net."
-    echo " "
-    echo "COPYRIGHT:"
-    echo "      Copyright © 2023 Free Software Foundation, Inc. This software is under the GPLv3+ license."
-    echo "      GNU GPL version 3 or later https://gnu.org/licenses/gpl.html."
-    echo "      This is free software: you are free to change and redistribute it. There is NO WARRANTY, to the extent"
-    echo "      permitted by law."
-}
 
 # ----------------------- Depricated for now -------------------------
 
