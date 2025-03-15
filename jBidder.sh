d6ad2eb15d969ea5abaaa1121f0ee5c59f0b669e
Patrik Eigenmann <patrik@Mac.attlocal.net>
2025-03-15 03:05:15 -0700

    newest changes

--- jBidder.sh
+++ jBidder.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Start building the classpath
-CLASSPATH="."
+#CLASSPATH="."
 #CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/Samael"
-CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/javax.activation_1.2.2.v20221203-1659.jar"
-CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/javax.mail.jar"
-CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/sqlite-jdbc-3.49.1.0.jar"
+#CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/javax.activation_1.2.2.v20221203-1659.jar"
+#CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/javax.mail.jar"
+#CLASSPATH="$CLASSPATH:/Users/patrik/Development/Java/extLib/sqlite-jdbc-3.49.1.0.jar"
 
 # Define the program's main class
-MAIN_CLASS="jBidder"
+#MAIN_CLASS="jBidder"
 
 # Start the program
-java -cp "$CLASSPATH" $MAIN_CLASS
+#java -cp "$CLASSPATH" $MAIN_CLASS
+cd /Users/patrik/bin || exit
+java -jar jBidder.jar
